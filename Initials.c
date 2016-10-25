5a2f171898ab397310d1be21c53cac170f2e4b71
SKovaS <SKovaS@users.noreply.github.com>
2016-10-25 18:50:54 +0200

    Update Initials.c

--- Initials.c
+++ Initials.c
@@ -8,16 +8,15 @@ int main (int argc, string argv[])
 	string input = "";
 	int length = 0;
 	
-	input = get_string();
-	length = strlen(input);
-	if(input[0]!=32){
-	printf("%c",toupper(input[0]));
+	input = get_string();//Вводим строку с клавиатуры.
+	length = strlen(input);//Длина строки.
+	if(input[0]!=32){//Если первый символ строки не пробел.
+	printf("%c",toupper(input[0]));//Выводим символ и переводим в заглавный если он строчный. 
 	}
-	int i;
-	for(i = 0; i <length; i++)
+	for(int i = 0; i <length; i++)
 	{
-		if((input[i-1] == 32)&&(input[i]!=32)) {
-			printf("%c",toupper(input[i]));
+		if((input[i-1] == 32)&&(input[i]!=32)) {//Проверяем следующий символ после пробела.
+			printf("%c",toupper(input[i]));//Выводим и переводим в заглавный следующий символ после пробела. 
 		
 		}
 	}
